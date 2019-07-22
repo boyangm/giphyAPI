9ca4a43a273edadb4135a9d23f6dec8c7a9124de
Boyang Matsapola <boyangbass@gmail.com>
2019-07-21 23:27:35 -0500

    got the buttons to add but the ajax wont load the pictures

---
+++ javascript/app.js
@@ -0,0 +1,54 @@
+window.onload = function(){
+let loadGif = ['dolphin', 'monkey', 'lizard', 'elephant', 'crab', 'frog', 'tutrle', 'dog', 'mouse', 'horse' ];
+const buttonArea= $('.buttonArea');
+const textArea = $('#textArea');
+const searchArea = $('.searchArea');
+const gifArea= $('.gifArea');
+
+
+function createButton (search){
+    const newButton = $('<button>')
+    newButton.attr('value', search).text(search);
+    buttonArea.append(newButton);
+    
+}
+function giphy(data){
+    data.map(item => {
+    let source= item.embed_url;
+    console.log(source);
+    const img = $('<img>');
+    img.attr("src", source);
+    gifArea.append(img);
+    })
+
+}
+function getResults(search){
+$.ajax({
+    method: 'GET',
+    url: `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=mquIIUiIFE2aPJyIIgWtpvnTxzj6ZKMv&limit=10&rating=PG`
+    })
+.then( response =>{
+    let data = response.data;
+    console.log(data);
+    giphy(data);
+    
+})
+
+}
+$('#submitButton').on('click', function (){
+    let query = textArea.val().trim();
+    event.preventDefault();
+    createButton(query);
+    getResults(query);
+    textArea.val('');
+    
+    console.log(query);
+    
+    
+})
+loadGif.map( elem =>{
+    createButton(elem);  
+})
+
+
+}
